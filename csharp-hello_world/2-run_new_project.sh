14788b7862c7e693d6e3fa2360ad31e3c8e12b56
kamyuwambaye <k.uwambaye@alustudent.com>
2026-09-09 00:09:20 +0200

    Add gitignore

--- csharp-hello_world/2-run_new_project.sh
+++ csharp-hello_world/2-run_new_project.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+rm -rf 2-new_project
 dotnet new console -o 2-new_project
+echo 'Console.WriteLine("Hello World!");' > 2-new_project/Program.cs
 dotnet build 2-new_project
 dotnet run --project 2-new_project
