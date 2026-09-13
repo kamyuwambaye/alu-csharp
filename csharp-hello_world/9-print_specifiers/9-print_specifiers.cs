7a31e6643800f1dee91a2ddafd5bd305f2e602fd
kamyuwambaye <k.uwambaye@alustudent.com>
2026-09-13 21:08:11 +0200

    Remove unexpected tip text from percent output

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine("Percent: {0:F2}% tip", percent * 100);
+		Console.WriteLine("Percent: {0:F2}%", percent * 100);
 		Console.WriteLine("Currency: {0}", currency.ToString("C2", System.Globalization.CultureInfo.GetCultureInfo("en-US")));
 	}
 }
