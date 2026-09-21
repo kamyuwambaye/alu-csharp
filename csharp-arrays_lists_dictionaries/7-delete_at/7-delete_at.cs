c66c32d00cff3b6fb64e9b37b9f96542b2858be7
kamyuwambaye <k.uwambaye@alustudent.com>
2026-09-21 22:45:19 +0200

    Complete delete at index exercise

---
+++ csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index < 0 || index >= myList.Count)
+        {
+            Console.WriteLine("Index is out of range");
+            return myList;
+        }
+
+        List<int> result = new List<int>();
+        for (int i = 0; i < myList.Count; i++)
+        {
+            if (i != index)
+            {
+                result.Add(myList[i]);
+            }
+        }
+        return result;
+    }
+}
