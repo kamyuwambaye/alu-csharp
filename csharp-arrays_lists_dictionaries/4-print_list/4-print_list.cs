ab18d82725868eb3a38dde1394cb6abc9bd862a4
kamyuwambaye <k.uwambaye@alustudent.com>
2026-09-21 22:35:05 +0200

    Complete print list exercise

---
+++ csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int>? CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+
+        List<int> list = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            list.Add(i);
+        }
+        for (int i = 0; i < size; i++)
+        {
+            Console.Write(list[i]);
+            if (i < size - 1)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+        return list;
+    }
+}
