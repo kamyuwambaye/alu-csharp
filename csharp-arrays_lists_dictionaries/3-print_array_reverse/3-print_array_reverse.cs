547f44d41f7e1d3c6ae8ef59c3fa4e94d02088c4
kamyuwambaye <k.uwambaye@alustudent.com>
2026-09-21 22:24:24 +0200

    Complete print array in reverse exercise

---
+++ csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,22 @@
+using System;
+
+class Array
+{
+    public static void Reverse(int[]? array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+        for (int i = array.Length - 1; i >= 0; i--)
+        {
+            Console.Write(array[i]);
+            if (i > 0)
+            {
+                Console.Write(" ");
+            }
+        }
+        Console.WriteLine();
+    }
+}
