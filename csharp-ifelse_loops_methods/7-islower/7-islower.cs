9fb37c35d01e99a4f1246c1c311ada22b6973236
kamyuwambaye <k.uwambaye@alustudent.com>
2026-09-21 07:33:07 +0200

    Complete islower character check exercise

---
+++ csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,13 @@
+using System;
+
+class Character
+{
+    public static bool IsLower(char c)
+    {
+        if (c >= 'a' && c <= 'z')
+        {
+            return true;
+        }
+        return false;
+    }
+}
