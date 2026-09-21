5898a7cf195b8d17e07acd33481f405a1957ab57
kamyuwambaye <k.uwambaye@alustudent.com>
2026-09-21 07:18:36 +0200

    Complete print alphabet exercise

---
+++ csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        for (char c = 'a'; c <= 'z'; c++)
+        {
+            Console.Write(c);
+        }
+    }
+}
